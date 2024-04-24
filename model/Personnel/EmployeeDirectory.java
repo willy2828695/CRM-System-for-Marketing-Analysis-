1bb9bad895410f49387a46b6c1c490510739c4a0
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-04-23 20:13:10 -0400

    Update EmployeeDirectory.java

--- model/Personnel/EmployeeDirectory.java
+++ model/Personnel/EmployeeDirectory.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model.Personnel;
 
 import java.util.ArrayList;
 
 import model.Business.Business;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class EmployeeDirectory {
 
     Business business;
