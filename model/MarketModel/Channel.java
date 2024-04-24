729aca059b1d7f83e89eedf3f94363e3802f54bd
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-04-23 20:12:10 -0400

    Update Channel.java

--- model/MarketModel/Channel.java
+++ model/MarketModel/Channel.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model.MarketModel;
 
 import java.util.ArrayList;
 
 import model.ProductManagement.SolutionOffer;
 
-/**
- *
- * @author kal bugrara
- */
+
 public class Channel {
     String name;
     
