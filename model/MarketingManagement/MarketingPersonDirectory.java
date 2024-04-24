717315a04fb88d9f03ef3b5db22cda2dba62aa69
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2024-04-23 20:12:39 -0400

    Update MarketingPersonDirectory.java

--- model/MarketingManagement/MarketingPersonDirectory.java
+++ model/MarketingManagement/MarketingPersonDirectory.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model.MarketingManagement;
 
 import java.util.ArrayList;
@@ -10,10 +6,6 @@ import java.util.ArrayList;
 import model.Business.Business;
 import model.Personnel.Person;
 
-/**
- *
- * @author kal bugrara
- */
 public class MarketingPersonDirectory {
 
     Business business;
